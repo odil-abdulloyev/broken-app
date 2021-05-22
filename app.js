20a38281e1834bf7d2d3e0e34605b5b34ae7a908
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-05-23 02:01:12 +0500

    fix: call app.listen after sync completed

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const game = require('./controllers/gamecontroller')
 
 const PORT = 4000;
 
-db.sync();
 app.use(require('body-parser').json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(PORT,function() {
-    console.log(`App is listening on port ${PORT}`);
-})
+
+db.sync().then(() => {
+    app.listen(PORT,function() {
+        console.log(`App is listening on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log(err.message);
+});
