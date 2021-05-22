d927cb6baffa78d2e0e9682a94b2b4c22171fd54
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-05-23 02:05:15 +0500

    fix: fix deprecated warning

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    operatorsAliases: false
 });
 
 sequelize.authenticate().then(
