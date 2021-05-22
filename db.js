361cc5c0f52e93c43bb5d77e136ecd0350043a99
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-05-22 23:06:13 +0500

    fix: setup sequelize

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
-                                //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
+
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres'
-})
+});
 
 sequelize.authenticate().then(
     function success() {
