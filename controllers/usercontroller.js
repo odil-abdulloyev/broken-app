57eeb8da6d12574f315f08abf8c9271f7e264846
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-05-23 19:13:38 +0500

    fix: fix several users with the same username error

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -6,17 +6,21 @@ const jwt = require('jsonwebtoken');
 const User = require('../db').import('../models/user');
 
 router.post('/signup', async (req, res) => {
+  const { username } = req.body.user;
   try {
+    if (await User.findOne({ where: { username } })) {
+      return res.status(400).send({ error: `User with username ${username} exists` });
+    }
     const user = await User.create({
       full_name: req.body.user.full_name,
-      username: req.body.user.username,
+      username,
       passwordHash: bcrypt.hashSync(req.body.user.password, 10),
       email: req.body.user.email,
     });
     const token = jwt.sign({id: user.id}, 'lets_play_sum_games_man', {expiresIn: 60 * 60 * 24});
     res.status(200).json({
-      user: user,
-      token: token
+      user,
+      token
     });
   } catch (err) {
     res.status(500).send(err.message);
@@ -31,16 +35,16 @@ router.post('/signin', async (req, res) => {
         if (matches) {
           const token = jwt.sign({id: user.id}, 'lets_play_sum_games_man', {expiresIn: 60 * 60 * 24});
           res.json({
-            user: user,
-            message: "Successfully authenticated.",
+            user,
+            message: 'Successfully authenticated.',
             sessionToken: token
           });
         } else {
-          res.status(502).send({error: "Passwords do not match."})
+          res.status(502).send({error: 'Passwords do not match.'})
         }
       });
     } else {
-      res.status(403).send({error: "User not found."})
+      res.status(403).send({error: 'User not found.'})
     }
   } catch (err) {
     res.status(500).send({error: err.message});
